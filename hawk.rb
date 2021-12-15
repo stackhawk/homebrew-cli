b6ec5055f0a08df03ef5155a3363e1dab743db72
wapmon <wapmon@aol.com>
2021-12-15 11:40:30 -0700

    first commit

---
+++ hawk.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Hawk < Formula
+  desc ""
+  homepage ""
+  url "https://download.stackhawk.com/hawk/cli/hawk-2.0.0-beta.zip"
+  sha256 "0c95243c3bf58ec71ca407d89a4c8dd3cf27ad25bc3651a6d978283732573892"
+  license ""
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    inreplace "hawk", "$(dirname $0)", "#{prefix}"
+    bin.install "hawk"
+    prefix.install Dir["./*"]
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test hawk`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
