114e85dce9c88ed5eb8a94f358dacad032cde746
root <root@67b0a4ed940f>
2022-04-26 21:05:17 +0000

    Updating brew's hawk.rb to version 2.3.1-dev

---
+++ hawk-dev.rb
@@ -0,0 +1,23 @@
+class HawkDev < Formula
+  desc "KaaKaww! Helping developers to find, triage and fix security bugs!"
+  homepage "https://www.stackhawk.com/"
+  url "https://download.test.stackhawk.com/hawk/cli/hawk-2.3.1.zip"
+  sha256 "d14e71bb3a1461b936508e9e132b2d8d4f4dabb82bd34ca65f65d20212150a8f"
+  license ""
+
+  def install
+    begin
+      system("java", "-version")
+    rescue
+      puts "Java was not found in $PATH.\n\nRun \e[1mbrew install openjdk\e[0m to install java\n\nHelp: https://docs.stackhawk.com/stackhawk-cli/#prerequisites\n "
+      raise "Java was not found"
+    end
+    inreplace "hawk", "$(dirname $0)", "#{prefix}"
+    bin.install "hawk"
+    prefix.install Dir["./*"]
+  end
+
+  test do
+    system "true"
+  end
+end
