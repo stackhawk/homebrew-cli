14cc2dd5d44ddba3f8863cc7a0e8eb9a737d47d6
root <root@90d4ff889236>
2022-05-09 18:10:39 +0000

    Updating brew's hawk.rb to version 2.3.2

--- hawk-dev@2.3.2.rb
+++ hawk-dev@2.3.2.rb
@@ -2,7 +2,7 @@ class HawkDevAT232 < Formula
   desc "KaaKaww! Helping developers to find, triage and fix security bugs!"
   homepage "https://www.stackhawk.com/"
   url "https://download.test.stackhawk.com/hawk/cli/hawk-2.3.2.zip"
-  sha256 "850f6e85a6797ede4ca3df3606db25f8d4248d5c11927f03e2fa33f7c2acda6f"
+  sha256 "12fcd9373702f5d88836c53be0dc0d63c43a43f8d173320338d04bc358fd7298"
   license ""
 
   def install
