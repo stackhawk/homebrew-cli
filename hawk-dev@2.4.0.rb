99697c2a4a0b8a7d8fb4fbbc4cf7cd6e9919fc91
root <root@7f6aa48bcbfa>
2022-06-06 22:05:22 +0000

    Updating brew's hawk.rb to version 2.4.0

--- hawk-dev@2.4.0.rb
+++ hawk-dev@2.4.0.rb
@@ -1,4 +1,4 @@
-class HawkDev < Formula
+class HawkDevAT240 < Formula
   desc "KaaKaww! Helping developers to find, triage and fix security bugs!"
   homepage "https://www.stackhawk.com/"
   url "https://download.test.stackhawk.com/hawk/cli/hawk-2.4.0.zip"
