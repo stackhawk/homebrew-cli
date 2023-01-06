2db125a1ae256fc99d51c480826705bd935bcca2
runner <runner@biodome-runner-amd64-qzqdw-gbcnj>
2023-01-06 19:47:22 +0000

    Updating brew's hawk.rb to version 2.10.0

--- hawk@2.10.0.rb
+++ hawk@2.10.0.rb
@@ -1,4 +1,4 @@
-class Hawk < Formula
+class HawkAT2100 < Formula
   desc "KaaKaww! Helping developers to find, triage and fix security bugs!"
   homepage "https://www.stackhawk.com/"
   url "https://download.stackhawk.com/hawk/cli/hawk-2.10.0.zip"
