f4318a69cbd134cfb8abd8987671f883b098bbf6
Samuel Volin <untra.sam@gmail.com>
2023-03-09 15:59:42 -0700

    Update hawk@2.12.0.rb

--- hawk@2.12.0.rb
+++ hawk@2.12.0.rb
@@ -1,7 +1,7 @@
 class HawkAT2120 < Formula
   desc "KaaKaww! Helping developers to find, triage and fix security bugs!"
   homepage "https://www.stackhawk.com/"
-  url "https://download.stackhawk.com/hawk/cli/hawk-${version}.zip"
+  url "https://download.stackhawk.com/hawk/cli/hawk-2.12.0.zip"
   sha256 "961f52db36abfd274d1a7aee0d384eca77f3c181a205c119ec01b51b4029bdb6"
   license ""
 
