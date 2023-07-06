b17055a9acda504be8ceb591bf0fcb69c80d9725
runner <runner@biodome-runner-amd64-4zkqb-zjb64>
2023-07-06 16:46:40 +0000

    Updating brew's hawk.rb to version 3.2.0

--- hawk@3.2.0.rb
+++ hawk@3.2.0.rb
@@ -1,4 +1,4 @@
-class Hawk < Formula
+class HawkAT320 < Formula
   desc "KaaKaww! Helping developers to find, triage and fix security bugs!"
   homepage "https://www.stackhawk.com/"
   url "https://download.stackhawk.com/hawk/cli/hawk-3.2.0.zip"
