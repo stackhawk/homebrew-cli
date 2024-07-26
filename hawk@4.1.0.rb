7d053515ee940a58db6336c0eeb521c74b832f38
runner <runner@biodome-runner-amd64-2b27f-cbd2k>
2024-07-26 21:31:22 +0000

    Updating brew's hawk.rb to version 4.1.0

--- hawk@4.1.0.rb
+++ hawk@4.1.0.rb
@@ -1,4 +1,4 @@
-class Hawk < Formula
+class HawkAT410 < Formula
   desc "KaaKaww! Helping developers to find, triage and fix security bugs!"
   homepage "https://www.stackhawk.com/"
   url "https://download.stackhawk.com/hawk/cli/hawk-4.1.0.zip"
