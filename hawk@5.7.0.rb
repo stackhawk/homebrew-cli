e75bd1a556263c6213a1172620a31bc654a88f54
runner <runner@biodome-amd-4cpu-sc9bg-runner-pjnmv>
2026-06-10 16:29:35 +0000

    Updating brew's hawk.rb to version 5.7.0

--- hawk@5.7.0.rb
+++ hawk@5.7.0.rb
@@ -1,4 +1,4 @@
-class Hawk < Formula
+class HawkAT570 < Formula
   desc "KaaKaww! Helping developers to find, triage and fix security bugs!"
   homepage "https://www.stackhawk.com/"
   url "https://download.stackhawk.com/hawk/cli/hawk-5.7.0.zip"
