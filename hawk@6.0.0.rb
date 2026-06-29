ec1c6aba8b8c81af73168955b0f37d3f14dd6258
runner <runner@biodome-amd-8cpu-9h7rj-runner-pzkwj>
2026-06-29 23:32:41 +0000

    Updating brew's hawk.rb to version 6.0.0

--- hawk@6.0.0.rb
+++ hawk@6.0.0.rb
@@ -1,4 +1,4 @@
-class Hawk < Formula
+class HawkAT600 < Formula
   desc "KaaKaww! Helping developers to find, triage and fix security bugs!"
   homepage "https://www.stackhawk.com/"
   version "6.0.0"
